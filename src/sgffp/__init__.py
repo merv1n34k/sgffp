4d79a497e7347aad3d937e81c2ec6efd2a64bf3a
Oleksii Stroganov <merv1n@proton.me>
2026-01-21 15:11:59 +0200

    fix: update modules access

--- src/sgffp/__init__.py
+++ src/sgffp/__init__.py
@@ -4,7 +4,8 @@ SnapGene File Format (SGFF) parser and writer
 
 from .reader import SgffReader
 from .writer import SgffWriter
-from .internal import SgffObject
+from .internal import SgffObject, Cookie, BlockList
 
-__all__ = ["SgffReader", "SgffWriter", "SgffObject"]
-__version__ = "0.1.0"
+__all__ = ["SgffReader", "SgffWriter", "SgffObject", "Cookie", "BlockList"]
+__version__ = "0.6.0"
+
